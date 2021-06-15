f3c74ac8f786988118fd7d60efca8342060d7edd
Gianni caycho <caycho1797giannina@gmail.com>
2021-06-15 18:32:00 -0500

    para mostrar los fails en el  home

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,22 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using pc4.Data;
 using pc4.Models;
 
 namespace pc4.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
+
         public IActionResult PaginaPrincipal()
         {
-            return View();
+            var fails = _context.Fails.Where(x => x.Fecha.AddDays(5) >= DateTime.Now).ToList();
+            return View(fails);
         }
 
 
